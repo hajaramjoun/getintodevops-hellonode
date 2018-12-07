28da22c92aa9883a3446487c07613cb29ee9f3cd
Amjoun Hajar <hajar.amjoun@yajade.com>
2018-12-07 09:51:10 +0100

    MAJ

--- DockerStart.sh
+++ DockerStart.sh
@@ -6,6 +6,6 @@ unzip $1 -d Rjcms
 else
    echo "Not Empty"
 docker build -t apachetom .
-docker run -d --name myContainer -p  8000:8080 apachetom
+docker run -d --name $2 -p  8000:8080 apachetom
  #! rm -rf Rjcms
 fi

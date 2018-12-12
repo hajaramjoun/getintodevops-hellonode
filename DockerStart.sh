3bb81b95c8314951d1000a39872efb5764ccafcf
Amjoun Hajar <hajar.amjoun@yajade.com>
2018-12-12 14:06:40 +0100

    MAJ

--- DockerStart.sh
+++ DockerStart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-[ ! -d Rjcms ] && mkdir -p Rjcms
+#[ ! -d Rjcms ] && mkdir -p Rjcms
 # unzip $1 -d Rjcms
-mv f Rjcms
+#mv f Rjcms
  if [ -z "$(ls -A Rjcms)" ]; then
    echo "Empty"
-else
-   echo "Not Empty"
+#else
+ #  echo "Not Empty"
 docker build -t apachetom .
-docker run -d --name $2 -p  8080:8080 apachetom
+#docker run -d --name $2 -p  8080:8080 apachetom
  #! rm -rf Rjcms
-fi
+#fi
